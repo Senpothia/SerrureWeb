650a7514c984ce393bfb8a6bd454fe51c5711b08
Senpothia <kintelalpz@gmail.com>
2021-04-04 18:15:50 +0200

    Ajout constantes de connexion

--- src/serrureweb/Constants/Constants.java
+++ src/serrureweb/Constants/Constants.java
@@ -1,10 +1,9 @@
-
 package serrureweb.Constants;
 
-
 public class Constants {
-    
-    public static String SERVER = "127.0.01";
+
+    public static String SERVER = "192.46.239.178";
+    public static String LOCAL = "127.0.01";
     public static int PORT = 5725;
-    
+
 }
