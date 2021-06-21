317db149fa15e84d9144f8def5c375c40234897d
Michel Lopez <michel@cdvi.com>
2021-06-21 16:43:58 +0200

    Correction adresse localhost

--- src/serrureweb/Constants/Constants.java
+++ src/serrureweb/Constants/Constants.java
@@ -2,8 +2,9 @@ package serrureweb.Constants;
 
 public class Constants {
 
-    public static String SERVER = "192.46.239.178";
-    public static String LOCAL = "127.0.01";
+    //public static String SERVER = "192.46.239.178";  // Serveur Linode
+    public static String SERVER = "46.105.35.7";       // Serveur Ubuntu_labo
+    public static String LOCAL = "127.0.0.1";
     public static int PORT = 6239;
 
 }
